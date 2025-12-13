dedb8985cfc4207f16ccb617843048a1189e6ff1
19r01a05g9-gif <19r01a05g9@gmail.com>
2025-12-13 13:36:57 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-te
+#include <QApplication>
+#include "MainWindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
